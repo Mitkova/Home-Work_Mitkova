5b22ddcfdccb2718288ec2d3b5fcef0f9f1f71a1
Mitkova <anjelamitkova@gmail.com>
2021-01-19 12:10:48 +0100

    tellStory

---
+++ JavaScript/Home Work - Class 04/1 HOMEWORK #1/Script/HW1.js
@@ -0,0 +1,15 @@
+let argument = ['"name"', '"mood"', '"activity"'];
+function tellStory() {
+    // let tellStory = ("My" + argument[0] + "is Andjla ");
+    let argumentName1 = "My "
+    let argumentName2 = " is Andjela."
+    let argumentMood1 = " Today I am in a good ";
+    let argumentMood2 = " and will go outside to do some sport "
+    let argumentActivity1 = "cause I study a lot of codes in now days, there is a little time left for sport " ;
+    let tellStory = argumentName1 + argument[0] + argumentName2 + argumentMood1 + argument[1] 
+         + argumentMood2 + argument[2] + ", " + argumentActivity1 + argument[2] + ".";
+    return tellStory;
+}
+console.log(tellStory());
+document.write(tellStory());
+window.alert(tellStory());
