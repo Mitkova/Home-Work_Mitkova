fe996b7324436c1535ef7469d6287f137357db99
Mitkova <anjelamitkova@gmail.com>
2021-04-04 23:32:32 +0200

    .

---
+++ 3. C# - Basic/Class 03/HomeWork.Class03/SumOfEven/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            int sum = 0;
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.WriteLine($"Enter integer {i} number :  ");
+                string userInput = Console.ReadLine();
+                bool isValidNumber = int.TryParse(userInput, out int number);
+                if (isValidNumber)
+                {
+                    //Console.WriteLine($"The {i} inserted number is {number}");
+                    array[i - 1] = number;
+                    if(number % 2 == 0)
+                    {
+                        //sum += number; - Linija 22 i Linija 23 isti kod
+                        sum = number + i;
+                    }
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"The {i}nd inserted input could not be converted into a number. "+
+                        $"\n 0 will be taken as a value");
+                }
+            }
+            Console.WriteLine($"The sum of Even Numbers is: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 3. C# - Basic/Class 03/Task-2 - StudentsGroup/Task-2 - StudentsGroup/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StudentGroup.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] studentsG1 = new string[5] { "Anji", "Goran", "Blagoj", "Marina", "Nadica" };
+            string[] studentsG2 = new string[5] { "MIke", "Bob", "Lusi", "Frank", "John" };
+
+            Console.WriteLine($"Enter number  {1} for studentsG1 or number {2} for studentsG2");
+            string userInput = Console.ReadLine();
+
+            //int number = int.Parse(userInput);
+            //Console.WriteLine(number);
+
+            bool isValidNumber = int.TryParse(userInput, out int number);
+            if (isValidNumber)
+            {
+                if (number == 1)
+                {
+                    foreach (string array1 in studentsG1) ;
+                    Console.WriteLine($"The studets in G1 are: {array1} ");
+                }
+                else if (number == 2)
+                {
+                    foreach (string array2 in studentsG2) ;
+                    Console.WriteLine($"The studets in G2 are: {array2}");
+                }
+                else
+                {
+                    Console.WriteLine("You entered wrong character, enter number 1 or number 2");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
