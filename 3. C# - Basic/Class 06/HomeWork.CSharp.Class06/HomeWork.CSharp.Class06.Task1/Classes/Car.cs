6f720ba34c3110656f5aa6b6c7a54d0de31f9d0f
Mitkova <74566254+Mitkova@users.noreply.github.com>
2021-04-12 21:59:16 +0200

    Add files via upload

---
+++ 3. C# - Basic/Class 06/HomeWork.CSharp.Class06/HomeWork.CSharp.Class06.Task1/Classes/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork.CSharp.Class06.Task1.Classes
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public Driver Driver { get; set; }
+
+        #region  Car Object   //Pocnav vaka da ja pravam so Objects, pa vidov nekoi drugi idei za resenia i prodolziv vo drug pravec so Arrays
+        //Car hyundai = new Car()
+        //{
+        //    Model = "Hyundai",
+        //    Speed = 150,
+        //    Driver = 1
+        //};
+
+        //Car mazda = new Car()
+        //{
+        //    Model = "Mazda",
+        //    Speed = 200,
+        //    Driver = 2
+        //};
+
+        //Car ferrari = new Car()
+        //{
+        //    Model = "Ferrari",
+        //    Speed = 340,
+        //    Driver = 3
+        //};
+
+        //Car porsche = new Car()
+        //{
+        //    Model = "Porsche",
+        //    Speed = 370,
+        //    Driver = 4
+        //};
+        #endregion 
+       
+        public Car()
+        {
+        }
+
+        public Car(string model, int speed)
+        {
+            Model = model;
+            Speed = speed;
+        }
+
+        public void SetDriver(Driver driver)
+        {
+            Driver = driver;
+        }
+        public int CalculateSpeed()
+        {
+            Car car = new Car();
+            car.Speed = int.Parse(Console.ReadLine());
+            return Driver != null ? Speed * Driver.Skill : 0;
+        }
+
+    }
+}
---
+++ 3. C# - Basic/Class 06/HomeWork.CSharp.Class06/HomeWork.CSharp.Class06.Task1/Program.cs
@@ -0,0 +1,79 @@
+﻿using HomeWork.CSharp.Class06.Task1.Classes;
+using System;
+
+namespace HomeWork.CSharp.Class06.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver[] drivers = new Driver[4]
+            {
+                new Driver("Andjlea", 10),
+                new Driver("Greg", 8),
+                new Driver("Jill", 6),
+                new Driver("Anne", 4),
+            };
+
+            Car[] cars = new Car[4]
+            {
+                new Car("Hyundai", 150),
+                new Car("Mazda", 180),
+                new Car("Ferrari", 250),
+                new Car("Porsche", 300)
+            };
+
+            Console.WriteLine("Select first car and driver");
+            int firstCarIndex = SelectCar(cars);
+            int firstDriverIndex = SelectDriver(drivers);
+
+            Car firstCar = cars[firstCarIndex];
+            Driver firstDriver = drivers[firstDriverIndex];
+            firstCar.SetDriver(firstDriver);
+
+            Console.WriteLine("Select second car and driver");
+            int secondCarIndex = SelectCar(cars, firstCar);
+            int secondDriverIndex = SelectDriver(drivers);
+
+            if (firstCarIndex == secondCarIndex)
+            {
+                Console.WriteLine("Error! You chouse the same car twice");
+                return;
+            }
+
+            if (firstDriverIndex == secondDriverIndex)
+            {
+                Console.WriteLine("Error! You chouse the same car twice");
+                return;
+            }
+
+            Car secondCar = cars[secondCarIndex];
+            Driver secondDriver = drivers[secondDriverIndex];
+            secondCar.SetDriver(secondDriver);
+
+            Car winner = RaceCars(firstCar, secondCar);
+
+            if (winner == null)
+            {
+                Console.WriteLine("Equal race!");
+                return;
+            }
+
+            Console.WriteLine($"The winner is {winner.Driver.Name} with {winner.Model} " +
+                $"with speed {winner.CalculateSpeed()}");
+
+            static Car RaceCars(Car car1, Car car2)
+            {
+                if (car1.CalculateSpeed() > car2.CalculateSpeed())
+                    return car1;
+                Console.WriteLine(car1 + " Was Faster");
+
+                if (car2.CalculateSpeed() > car1.CalculateSpeed())
+                    return car2;
+                return null;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
