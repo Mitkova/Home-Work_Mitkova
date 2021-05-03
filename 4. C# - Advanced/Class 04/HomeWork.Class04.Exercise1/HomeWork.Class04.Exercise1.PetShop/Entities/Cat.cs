1411d88eb83262e56eb15cf67d3fe64958f9db06
Mitkova <74566254+Mitkova@users.noreply.github.com>
2021-05-03 12:38:03 +0200

    Add files via upload

---
+++ 4. C# - Advanced/Class 04/HomeWork.Class04.Exercise1/HomeWork.Class04.Exercise1.PetShop/Entities/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork.Class04.Exercise1.PetShop.Entities
+{
+    public class Cat : Pet
+    {
+        public bool Lazy {get; set;}
+        public int LivesLeft {get; set;}
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Cat {Name} has {LivesLeft} lives left!");
+        }
+    }
+}
---
+++ 4. C# - Advanced/Class 04/HomeWork.Class04.Exercise1/HomeWork.Class04.Exercise1.PetShop/Entities/PetStore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork.Class04.Exercise1.PetShop.Entities
+{
+    public class PetStore<T> where T : Pet
+    {
+        private List<T> DB { get; set; }
+
+        public PetStore()
+        {
+            DB = new List<T>();
+        }
+
+        public void Insert (T item)
+        {
+            DB.Add(item);
+            Console.WriteLine("Item was added to DB!");
+        }
+
+
+        public void PrintPets()
+        {
+            foreach (T item in DB)
+            {
+                item.PrintInfo();
+            }
+        }
+
+        public void BuyPet (string name)
+        {
+            T pet = DB.FirstOrDefault(x => x.Name == name);
+            if (pet == null)
+            {
+                Console.WriteLine("No such pet");
+                return;
+            }
+            DB.Remove(pet);
+            Console.WriteLine("Conngrats you have bought a pet!");
+        }
+
+    }
+}
